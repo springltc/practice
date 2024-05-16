d5f242eb34bb34e33039f8002482ac5bae779f2b
liutc <17681102655@163.com>
2024-05-16 13:50:10 +0800

    comit

--- coding/src/main/java/com/example/coding/lec/哈希/两数之和.java
+++ coding/src/main/java/com/example/coding/lec/哈希/两数之和.java
@@ -1,58 +1,17 @@
 package com.example.coding.lec.哈希;
 
 /**
- * @author liutc
- * @date 2024/2/6 11:22
- */
-
-import java.util.HashMap;
-import java.util.Objects;
-
-/**
  * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
  * <p>
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
  * <p>
  * 你可以按任意顺序返回答案。
  */
-public class 两数之和 {
-    public static void main(String[] args) {
-        int[] number = {3, 2, 4};
-        int target = 6;
-        int[] result;
-//        result = method1(number, target);
-        result = method2(number, target);
-        if (Objects.nonNull(result)) {
-            for (int i : result) {
-                System.out.println(i);
-            }
-        }
-    }
 
-    //思路 两层大循环
-    public static int[] method1(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-        return null;
-    }
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
-    //思路 哈希表
-    public static int[] method2(int[] nums, int target) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (hashMap.containsKey(target - nums[i])) {
-                return new int[]{hashMap.get(target - nums[i]), i};
-            }
-            hashMap.put(nums[i], i);
-        }
-        return null;
-    }
-}
 /**
  * 示例 1：
  * <p>
@@ -68,3 +27,34 @@ public class 两数之和 {
  * 输入：nums = [3,3], target = 6
  * 输出：[0,1]
  */
+public class 两数之和 {
+
+    public static void main(String[] args) {
+//        int[] nums = {2, 7, 11, 15};
+//        int target = 9;
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] ints = twoSum(nums, target);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(ints[i]);
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            Integer index = map.get(num);
+            if (Objects.nonNull(index)) {
+                result[0] = index;
+                result[1] = i;
+                return result;
+            } else {
+                map.put(target - num, i);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ coding/src/main/java/com/example/coding/lec/哈希/单词规律.java
@@ -0,0 +1,271 @@
+package com.example.coding.lec.哈希;
+
+/**
+ * @author liutc
+ * @date 2024/5/16 09:33
+ * <p>
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false
+ * <p>
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false
+ * <p>
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false
+ * <p>
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false
+ * <p>
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false
+ * <p>
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false
+ * <p>
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false
+ * <p>
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false
+ * <p>
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false
+ * <p>
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false
+ * <p>
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false
+ */
+
+/**
+ * 给定一种规律 pattern 和一个字符串 s ，判断 s 是否遵循相同的规律。
+ *
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 s 中的每个非空单词之间存在着双向连接的对应规律。
+ */
+
+/**
+ * 示例1:
+ *
+ * 输入: pattern = "abba", s = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入:pattern = "abba", s = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入: pattern = "aaaa", s = "dog cat cat dog"
+ * 输出: false
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * 提示:
+ *
+ * 1 <= pattern.length <= 300
+ * pattern 只包含小写英文字母
+ * 1 <= s.length <= 3000
+ * s 只包含小写英文字母和 ' '
+ * s 不包含 任何前导或尾随对空格
+ * s 中每个单词都被 单个空格 分隔
+ */
+public class 单词规律 {
+
+    public static void main(String[] args) {
+        String s = "dog cat cat dog";
+        String pattern = "abba";
+        boolean wordPattern = wordPattern(pattern, s);
+        System.out.println(wordPattern);
+    }
+
+    public static boolean wordPattern(String pattern, String s) {
+        String[] split = s.split(" ");
+        if (pattern.length() != split.length) {
+            return false;
+        }
+        Map<Character, String> map1 = new HashMap<>();
+        Map<String, Character> map2 = new HashMap<>();
+        for (int i = 0; i < pattern.length(); i++) {
+            char charAt = pattern.charAt(i);
+            String s1 = split[i];
+
+            String s2 = map1.get(charAt);
+            if (Objects.isNull(s2)) {
+                map1.put(charAt, s1);
+            } else {
+                if (!s1.equals(s2)) {
+                    return false;
+                }
+            }
+
+            Character character = map2.get(s1);
+            if (Objects.isNull(character)) {
+                map2.put(s1, charAt);
+            } else {
+                if (character != charAt) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
